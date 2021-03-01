8eb571c30ad9e44dada07bd0ba1daa413ccea452
Fabien Guichard <fguichard@fnetworks.tf>
2021-03-01 16:11:30 +0100

    Initialisation du projet wireguard FE/GCP

---
+++ wireguard/README.md
@@ -0,0 +1,15 @@
+# Hybridation cloud : terraform-wireguard
+
+Modules terraform pour deployer wireguard sur FE et GCP
+
+## Prerequis
+
+  * terraform >= 0.14      # Voir [versions.tf](versions.tf)
+  * provider FE >= 1.18    # Voir [providers.tf](providers.tf)
+  * Provider GCP => 3.58   # Voir [providers.tf](providers.tf)   
+  * projets FE + GCP
+  * credentials de conf FE + SA GCP
+  
+
+
+
---
+++ wireguard/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    flexibleengine = {
+      version = "~> 1.18"
+# Could not retrieve the list of available versions for provider hashicorp/fe:
+# provider registry registry.terraform.io does not have a provider named
+# registry.terraform.io/hashicorp/fe
+      source = "FlexibleEngineCloud/flexibleengine"
+    }
+    google = {
+      version = "~> 3.58"
+    }
+  }
+}
